ef26de0c1cf312d2bd06688d908b89b440c3be0c
karthikcode99 <85825128+karthikcode99@users.noreply.github.com>
2021-08-07 14:14:26 +0530

    Update for.js

--- for.js
+++ for.js
@@ -10,3 +10,33 @@ async function fetchUsers() {
 }
 
 fetchUsers();
+
+
+TASK 
+let arr = [1,2,3,4,5,6,7,8,9,10,11,12]
+
+let odds = arr.filter(n => n%2)
+
+console.log(odds)
+
+//prime number
+var numArray = [2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+numArray = numArray.filter((number) => {
+  for (var i = 2; i <= Math.sqrt(number); i++) {
+    if (number % i === 0) 
+      return false;
+  }
+  return true;
+});
+
+console.log(numArray);
+//caps
+function titleCase(str) {
+  str = str.toLowerCase().split(' ');
+  for (var i = 0; i < str.length; i++) {
+    str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1); 
+  }
+  console.log(str.join(' '))
+}
+titleCase("good work");
