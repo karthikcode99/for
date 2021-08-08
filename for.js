b30fca4c3e187de4044c8e97723c0ea02ab3c973
karthikcode99 <85825128+karthikcode99@users.noreply.github.com>
2021-08-08 10:19:51 +0530

    Update for.js

--- for.js
+++ for.js
@@ -61,3 +61,23 @@ numArray = numArray.filter((number) => {
 });
 
 console.log(numArray);
+// palindrome
+const arr = ['mom', 13445, 12321, 'dad', 'cannot'];
+const isPalindrome = el => {
+   const str = String(el);
+   let i = 0;
+   let j = str.length - 1;
+   while(i < j) {
+      if(str[i] === str[j]) {
+         i++;
+         j--;
+      } else {
+         return false;
+      }
+   }
+   return true;
+};
+const findPalindrome = arr => {
+   return arr.filter(el => isPalindrome(el));
+};
+console.log(findPalindrome(arr));
